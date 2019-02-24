0d0bbde7f0195a6fdbb7531635b8925f8ddbab6b
shen hong wen <610933351@qq.com>
2019-02-24 20:37:13 +0800

    fix author name shen->sShen

--- Python 100例/Python 练习实例1.py
+++ Python 100例/Python 练习实例1.py
@@ -1,4 +1,12 @@
-# -*- coding: UTF-8 -*-
+#!/usr/bin/env python
+# coding=UTF-8
+'''
+@Description: About practicing python exercises
+@Author: Shenhongwen
+@LastEditors: Shenhongwen
+@Date: 2019-02-24 20:32:16
+@LastEditTime: 2019-02-24 20:36:30
+'''
 for i in range(1, 5):
     for j in range(1, 5):
         for k in range(1, 5):
