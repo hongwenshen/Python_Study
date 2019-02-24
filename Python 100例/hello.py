e003dbaf400c76aaf5f4e73295329d278c07ce16
shen hong wen <610933351@qq.com>
2019-02-24 20:34:56 +0800

    0224 Test

---
+++ Python 100例/hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=UTF-8
+'''
+@Description: About practicing python exercises
+@Author: Shenhongwen
+@LastEditors: Shenhongwen
+@Date: 2019-02-24 18:38:10
+@LastEditTime: 2019-02-24 19:46:26
+'''
+
+msg = 'hello world'
+print(msg)
